2ecd030e842fa5cdc576003247b572bc2d7e240c
Marie Burns <mcburns2222@gmail.com>
2018-11-24 20:43:40 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -6,8 +6,8 @@ class Shoe
 
   def initialize(brand)
     @brand = brand
-    if BRANDS.each {|brand| brand != @brand}
-      BRANDS << @brand
+    if !BRANDS.include?(brand)
+      BRANDS << brand
     end
   end
 
